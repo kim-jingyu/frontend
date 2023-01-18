7150f881362b5bb7064593e1c6399385b2bf1623
jingyu9804 <swc05161@gmail.com>
2023-01-18 19:02:55 +0900

    feat(javascript) : js 연습

---
+++ class/03-javascript/01-variable.js
@@ -0,0 +1,11 @@
+  console.log("콘솔로그를 찍어봅니다.")
+
+//   변수
+  let name // undefined
+  name = "홍길동"
+  name = "김코딩" // 재할당
+  console.log(name)
+
+//   상수
+  const name1 = "홍길동"
+//   name1 = "스티브잡스" // 재할당 -> 오류 발생
