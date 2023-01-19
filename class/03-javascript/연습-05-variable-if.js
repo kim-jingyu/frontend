a9caf7f40854b3551114d7c45c819e740621e4f2
jingyu9804 <swc05161@gmail.com>
2023-01-19 19:59:11 +0900

    feat(javascript) : 조건문

--- class/03-javascript/연습-05-variable-if.js
+++ class/03-javascript/연습-05-variable-if.js
@@ -21,3 +21,52 @@ true || false
 // true
 !false
 // true
+
+// -----------------------------
+
+// 조건문 실습 1
+if ( 1+1 === 2 ){
+    console.log("정답")
+} else{
+    console.log("오답")
+}
+// VM302:2 정답
+// undefined
+if (!true){
+    console.log("정답")
+} else{
+    console.log("오답")
+}
+// VM365:4 오답
+// undefined
+if (0){
+    console.log("정답")
+} else{
+    console.log("오답")
+}
+// VM380:4 오답
+// undefined
+if (1){
+    console.log("정답")
+} else{
+    console.log("오답")
+}
+// VM420:2 정답
+
+// 조건문 실습2
+const profile = {
+    name: "철수",
+    age: 12,
+    school: "다람쥐초등학교"
+}
+// undefined
+if(profile.age >= 20){
+    console.log("성인입니다.")
+} else if(profile.age >= 8){    // 코드 최적화
+    console.log("학생입니다.")
+} else if(profile.age > 0){
+    console.log("어린이입니다.")
+} else{                         // 에러 핸들링
+    console.log("잘못 입력하셨습니다.")
+}
+// VM2316:4 학생입니다.
