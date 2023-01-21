36c4670f73d3b959a5da80b507d0ebb0e59b4838
jingyu9804 <swc05161@gmail.com>
2023-01-21 15:21:34 +0900

    feat(javascript) : timer - setInterval 함수 처리

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,21 +1,37 @@
+let isStarted = false
+
 const submit = () => {
-    let token = (String)((Math.floor(Math.random()*1000000))).padStart(6,"0")
-    document.getElementById("number").innerText = token
-    document.getElementById("number").style.color = "#" + token
+    if(isStarted === false){
+        // 타이머가 작동중이 아닐때
+        isStarted = true
 
-    let time = 3
+        document.getElementById("finish").disabled = false
+        let token = (String)((Math.floor(Math.random()*1000000))).padStart(6,"0")
+        document.getElementById("number").innerText = token
+        document.getElementById("number").style.color = "#" + token
+    
+        let time = 3
+        let timer
+    
+        timer = setInterval(function(){
+            
+            if(time>=0){
+                let min = Math.floor(time/60)
+                let sec = (String)(time%60).padStart(2,"0")
+                time = time - 1
+                document.getElementById("count").innerText = min + ":" + sec
+            }else{
+                document.getElementById("finish").disabled = true
+                isStarted = false
+                console.log("타이머가 작동중입니다.")
+                clearInterval(timer)
+            }
+    
+        },1000)
+    } else{
+        // 타이머가 작동중일때
 
-    setInterval(function(){
-        
-        if(time>=0){
-            let min = Math.floor(time/60)
-            let sec = (String)(time%60).padStart(2,"0")
-            time = time - 1
-            document.getElementById("count").innerText = min + ":" + sec
-        }else{
-            document.getElementById("finish").disabled = true
-        }
-
-    },1000)
+    }
+    
 
 }
