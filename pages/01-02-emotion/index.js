3d5b0d518e5c1f6a8bc986341c0d4f7faf5b9143
jingyu9804 <swc05161@gmail.com>
2023-03-15 04:06:27 +0900

    feat(class) : JSX, CSS

---
+++ pages/01-02-emotion/index.js
@@ -0,0 +1,15 @@
+import {Email, EmailInput} from '../../styles/emotion'
+
+export default function EmotionPage(){
+ 
+    // 자바스크립트 쓰는 곳
+
+    return(
+        <div>
+            <Email>이메일</Email>
+            <EmailInput type="text"/>
+            <button>클릭하세요~~</button>
+            <img src='/vercel.svg' />
+        </div>
+    )
+}
