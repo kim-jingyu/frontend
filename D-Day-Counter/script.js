f0540c83b13a90c047166b9811d6f52dc2d096a4
jingyu9804 <swc05161@gmail.com>
2023-03-18 21:02:32 +0900

    feat(Counter) : 남은 시간 화면에 뿌리기 기능

---
+++ D-Day-Counter/script.js
@@ -0,0 +1,48 @@
+const container = document.querySelector('#d-day-container')
+const messageContainer = document.querySelector('#d-day-message')
+
+container.style.display = 'none'
+messageContainer.innerHTML = 'D-Day를 입력해 주세요.'
+
+const dateFormMaker = () => {
+    const inputYear = document.querySelector("#target-year-input").value
+    const inputMonth = document.querySelector("#target-month-input").value
+    const inputDate = document.querySelector("#target-date-input").value
+
+    const dateForm = `${inputYear}-${inputMonth}-${inputDate}`
+    return dateForm
+}
+
+const counterMaker = () => {
+    const targetDateInput = dateFormMaker()
+    const nowDate = new Date()
+    const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0)
+    const remaining = (targetDate - nowDate) / 1000                     // ms 나눠주기
+
+    if(remaining <= 0){
+        // 만약 remaining이 0 또는 그 이하이면, 타이머가 종료되었습니다. 출력
+        // alert('타이머가 종료되었습니다.')
+        messageContainer.innerHTML = '타이머가 종료되었습니다.'
+    }else if(isNaN(remaining)){
+        // 만약 잘못된 날짜가 들어왔다면, 유효한 시간대가 아닙니다. 출력
+        // alert('유효한 시간대가 아닙니다.')
+        messageContainer.innerHTML = '유효한 시간대가 아닙니다.'
+    }
+
+    const remainingDate = Math.floor(remaining / 3600 / 24)     // 남은 날짜
+    const remainingHours = Math.floor(remaining / 3600) % 24    // 남은 시간
+    const remainingMin = Math.floor(remaining / 60) % 60        // 남은 분
+    const remainingSec = Math.floor(remaining) % 60             // 남은 초
+
+    const days = document.getElementById('days')                // 남은 날짜
+    const hours = document.getElementById('hours')              // 남은 시간
+    const min = document.getElementById('min')                  // 남은 분
+    const sec = document.getElementById('sec')                  // 남은 초
+
+    days.textContent = remainingDate
+    hours.textContent = remainingHours
+    min.textContent = remainingMin
+    sec.textContent = remainingSec
+
+    console.log("남은 시간 : " + remainingDate +"일, "+ remainingHours +"시, "+ remainingMin +"분, "+ remainingSec +"초")
+}
