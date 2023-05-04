6f44a3c8c861cb1dcc27b70db0b6b91b5ccb70fd
jingyu9804 <swc05161@gmail.com>
2023-05-05 01:07:35 +0900

    feat(class): 함수형 컴포넌트 vs 클래스형 컴포넌트

--- pages/01-01-qqq/index.js
+++ pages/01-01-qqq/index.js
@@ -1,9 +1,32 @@
-export default function Qqq(){
+// export default function Qqq(){
 
-    return(
-        <div>
-            <div>철수</div>
-            <button>클릭하세요~~</button>
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             <div>철수</div>
+//             <button>클릭하세요~~</button>
+//         </div>
+//     )
+// }
+
+// 클래스형 컴포넌트 작성법
+// import { Component } from "react"
+
+// class New extends Component {
+//     render(){
+//         return <div>이것은 클래스형 컴포넌트</div>
+//     }
+// }
+
+// export default New
+
+// 함수형 컴포넌트 작성법 1
+// function New(){
+//     return <div>이것은 함수형 컴포넌트</div>
+// }
+
+// export default New
+
+// 함수형 컴포넌트 작성법 2
+// const New = () => <div>이것은 함수형 컴포넌트</div>
+
+// export default New
