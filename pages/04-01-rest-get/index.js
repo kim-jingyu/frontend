a8634bd21d983b2fb8920de52bb1fa785a385ce0
jingyu9804 <swc05161@gmail.com>
2023-05-07 00:32:09 +0900

    feat(class): 호이스팅을 고려 + REST-API 동기/비동기 통신

---
+++ pages/04-01-rest-get/index.js
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { useState } from "react"
+
+export default function RestGetPage() {
+
+    // state
+    const [title, setTitle] = useState("") // 초기값 = ""
+    
+
+    // 이벤트 핸들러 함수
+
+    // 비동기 함수
+    const onClickAsync = () => {
+        const result = axios.get('https://koreanjson.com/posts/1')
+        console.log(result)
+    }
+
+    // 동기 함수
+    const onClickSync = async () => {
+        const result = await axios.get('https://koreanjson.com/posts/1')
+        console.log(result)
+        console.log(result.data)
+        console.log(result.data.title)
+        setTitle(result.data.title)
+    }
+
+    return(
+        <>
+            {/* 비동기 버튼 */}
+            <button onClick={onClickAsync}>Rest-API 비동기 방식으로 요청하기</button>
+
+            {/* 동기 버튼 */}
+            <button onClick={onClickSync}>Rest-API 동기 방식으로 요청하기</button>
+
+            <div>{title}</div>
+        </>
+        
+    )
+}
