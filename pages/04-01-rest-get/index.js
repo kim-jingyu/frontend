1b3505f8944560b1315be7f75026775690b23c76
jingyu9804 <swc05161@gmail.com>
2023-05-07 21:01:14 +0900

    feat(class): _app.js는 모든 파일의 공통 설정 페이지이다.

--- pages/04-01-rest-get/index.js
+++ pages/04-01-rest-get/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useState } from "react"
+import Login from '../02-03-signup-state'
 
 export default function RestGetPage() {
 
@@ -28,11 +29,12 @@ export default function RestGetPage() {
         <>
             {/* 비동기 버튼 */}
             <button onClick={onClickAsync}>Rest-API 비동기 방식으로 요청하기</button>
-
             {/* 동기 버튼 */}
             <button onClick={onClickSync}>Rest-API 동기 방식으로 요청하기</button>
-
             <div>{title}</div>
+
+            {/* 컴포넌트 조립 방식 */}
+            <Login></Login>
         </>
         
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,24 @@
-// 세팅하기
+// 모든 파일 공통 세팅하기
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 export default function App({ Component, pageProps }) {
 
+  // graphql 세팅
   const client = new ApolloClient({
     // API 있는 위치를 모든 페이지에 알려주기 위해 세팅
     uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache()  // 나중에 꺼내서 쓰기 위해 컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장
   })
 
   return(
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />  
-    </ApolloProvider>
+    <div>
+      <div>=======여기는 _app.js 컴포넌트 시작 부분입니다.==========</div>
+      {/* ApolloProvider -> 이 컴포넌트에서 client graphql 세팅을 사용할 수 있게 전달해준다. */}
+      <ApolloProvider client={client}>
+        {/* 각 페이지 컴포넌트 */}
+        <Component {...pageProps} />  
+      </ApolloProvider>
+      <div>=======여기는 _app.js 컴포넌트 마지막 부분입니다.==========</div>
+    </div>
   ) 
 }
