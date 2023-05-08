366c2218ecc8ded22bf0db8c1b6851ce52e6dd5f
jingyu9804 <swc05161@gmail.com>
2023-05-09 04:53:19 +0900

    feat(class): 컴포넌트 방식으로 폴더 구조 나누기 - container, presenter

---
+++ src/components/units/board/write/BoardWrite.presenter.js
@@ -0,0 +1,17 @@
+import {RedInput,BlueButton} from './BoardWrite.styles'
+
+export default function BoardWriteUI(props) {
+
+    return(
+        <div>
+            <div>===========여기는 presenter입니다.===========</div>
+            <div>
+                작성자: <RedInput type="text" onChange={props.onChangeWriter}></RedInput>
+                제목: <RedInput type="text" onChange={props.onChangeTitle}></RedInput>
+                내용: <RedInput type="text" onChange={props.onChangeContents}></RedInput>
+                <BlueButton onClick={props.onClickSubmit}>graphql-API 동기 요청하기</BlueButton>
+            </div>
+            <div>===========여기는 presenter입니다.===========</div>
+        </div>
+    )
+}
