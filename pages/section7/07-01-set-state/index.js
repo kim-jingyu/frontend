014a6cea0de0beaa41d3e15a0c6fd3c0ae2eb8c3
jingyu9804 <swc05161@gmail.com>
2023-05-09 21:44:41 +0900

    chore(class): 주석 추가

--- pages/section7/07-01-set-state/index.js
+++ pages/section7/07-01-set-state/index.js
@@ -4,7 +4,7 @@ export default function GraphqlMutationPage() {
     // 한 줄일때는 괄호 필요 없음
     return(
         <div>
-            <div>#########여기는 페이지입니다.#########</div>
+            <div>~~~~~~~~여기는 페이지입니다.~~~~~~~~</div>
             <BoardWrite />
             <div>#########여기는 페이지입니다.#########</div>
         </div>
