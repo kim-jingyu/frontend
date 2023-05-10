a074eda023b761859c2f8c2aea95604431ce94f1
jingyu9804 <swc05161@gmail.com>
2023-05-10 21:05:00 +0900

    feat(class): 게시글 등록,수정 페이지 만들기v1 - 비효율적

---
+++ pages/section9/09-01-board-edit/index.js
@@ -0,0 +1,11 @@
+// 게시글 수정
+export default BoardEditPage = () => {
+    return(
+        <>
+            <h1>수정페이지</h1>
+            제목: <input type="text" /><br/>
+            내용: <input type="text" /><br/>
+            <button>수정하기</button>
+        </>
+    )
+}
