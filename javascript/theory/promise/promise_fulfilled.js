baca309eb46c209ebba117d4e3e4f95e23ad04ca
kim-jingyu <swc05161@gmail.com>
2024-01-13 20:15:00 +0900

    Revert "feat: promise_pending, promise_fulfilled"
    This reverts commit 173fb17c7bb79e855787e8693eec8fed5ec02da2.

--- javascript/theory/promise/promise_fulfilled.js
+++
@@ -1,8 +0,0 @@
-const promise = new Promise((resolve, reject) => {
-    resolve("처리 완료");
-});
-
-promise
-    .then((data) => {
-        console.log("Promise Full-Filled 처리 결과에 대한 데이터 출력 => ", data);
-    })
